8f273495c8f4d4b2b1178b32f47a5d2fcea58ce7
Divyagnan <divyagnan@users.noreply.github.com>
2017-08-21 13:29:55 -0400

    tests: add test to make sure UTC option works for zero date strings

--- __tests__/Date.test.tsx
+++ __tests__/Date.test.tsx
@@ -6,6 +6,7 @@ import { DateFormatter as DateComponent } from "../src/Date";
 
 const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
 const dateString = "2015-03-25T12:00:00Z";
+const zeroDateString = "2015-03-01T00:00:00z";
 
 describe("Date Formatter", () => {
   it("does basic formatting of a date- without config", () => {
@@ -23,6 +24,30 @@ describe("Date Formatter", () => {
       </DateComponent>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  it("does basic formatting of a zero date string", () => {
+    /**
+     * if you have a date string as follows "2015-03-01T00:00:00z"
+     * in some locales you will see a date that is one day before
+     * in other words the date string above with no UTC option will
+     * show 2/28/2015 rather than 3/1/2015. Providing the UTC option
+     * will give the expected date.
+     */
+
+    // "wrong" date - should equal 2/28/2015
+    const noUTCwrapper = mount(
+      <DateComponent>
+        {zeroDateString}
+      </DateComponent>
+    );
+    expect(noUTCwrapper.html()).toEqual("<span>2/28/2015</span>");
+    // "correct" date - should equal 3/1/2015
+    const UTCwrapper = mount(
+      <DateComponent options={{ timeZone: "UTC" }}>
+        {zeroDateString}
+      </DateComponent>
+    );
+    expect(UTCwrapper.html()).toEqual("<span>3/1/2015</span>");
   });
   it("is responsive to locale changes", () => {
     const wrapper = mount(
